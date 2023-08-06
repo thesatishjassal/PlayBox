bc8d3c7ccc0806a65dbdcd52ba56609ac63bba77
thesatishjassal <thesatishjassal@gmail.com>
2023-08-07 02:22:05 +0530

    size updated for loading card

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -9,7 +9,7 @@ import {
 
 export const GameCardSkeleton = () => {
   return (
-    <Card borderRadius={10} padding="6">
+    <Card width="100%" borderRadius={10} padding="6">
         <Skeleton height="180px" size="10"></Skeleton>
         <SkeletonText mt="4" noOfLines={2} spacing="4" skeletonHeight="2" />
     </Card>
